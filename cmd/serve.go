503e0acd75b74846efda2d8789cb6cb9ade50a67
Bicky Tamang <33925230+bicky-tmg@users.noreply.github.com>
2023-07-24 19:33:59 +0545

    creating routing package

--- cmd/serve.go
+++ cmd/serve.go
@@ -2,7 +2,7 @@ package cmd
 
 import (
 	"WriteWise/pkg/config"
-	"fmt"
+	"WriteWise/pkg/routing"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -26,14 +26,16 @@ var serveCmd = &cobra.Command{
 func serve() {
 	config.Set()
 
-	configs := config.Get()
+	routing.Init()
 
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := routing.GetRouter()
+
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message":  "pong",
 			"app name": viper.Get("App.Name"),
 		})
 	})
-	r.Run(fmt.Sprintf("%s:%s", configs.Server.Host, configs.Server.Port)) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	routing.Serve()
 }
