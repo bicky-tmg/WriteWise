7fb0a0fe35d22df4fe6a37d2bce5e043c9f3729c
Bicky Tamang <33925230+bicky-tmg@users.noreply.github.com>
2023-07-25 08:10:14 +0545

    creating html pkg

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -2,6 +2,7 @@ package bootstrap
 
 import (
 	"WriteWise/pkg/config"
+	"WriteWise/pkg/html"
 	"WriteWise/pkg/routing"
 )
 
@@ -9,6 +10,8 @@ func Serve() {
 	config.Set()
 
 	routing.Init()
+
+	html.LoadHTML(routing.GetRouter())
 
 	routing.RegisterRoutes()
 
---
+++ pkg/html/render.go
@@ -0,0 +1,13 @@
+package html
+
+import (
+	"WriteWise/internal/providers/view"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Render(c *gin.Context, code int, name string, data gin.H) {
+	data = view.WithGlobalData(data)
+
+	c.HTML(code, name, data)
+}
