3f490fd7f29d6b9ad6df5f4c27655c687caf5659
Bicky Tamang <33925230+bicky-tmg@users.noreply.github.com>
2023-07-25 08:31:39 +0545

    creating static packages

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -4,12 +4,15 @@ import (
 	"WriteWise/pkg/config"
 	"WriteWise/pkg/html"
 	"WriteWise/pkg/routing"
+	"WriteWise/pkg/static"
 )
 
 func Serve() {
 	config.Set()
 
 	routing.Init()
+
+	static.LoadStatic(routing.GetRouter())
 
 	html.LoadHTML(routing.GetRouter())
 
