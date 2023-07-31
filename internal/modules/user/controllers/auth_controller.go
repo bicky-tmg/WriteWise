fb06be2591a149d0ca4610655152b95e59560cb8
Bicky Tamang <33925230+bicky-tmg@users.noreply.github.com>
2023-07-31 17:07:40 +0545

    Handle register form part 1

--- internal/modules/user/controllers/auth_controller.go
+++ internal/modules/user/controllers/auth_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"WriteWise/internal/modules/user/requests/auth"
 	"WriteWise/pkg/html"
 	"net/http"
 
@@ -18,5 +19,17 @@ func (ctrl *Controller) Register(c *gin.Context) {
 }
 
 func (ctrl *Controller) HandleRegister(c *gin.Context) {
+	// validate the request
+	var request auth.RegisterRequest
+	// This will infer what binder to use depending on the content-type header.
+	if err := c.ShouldBind(&request); err != nil {
+		c.Redirect(http.StatusFound, "/register")
+		return
+	}
+	// Create the user
+
+	// Check if there is any error on the user creation
+
+	// after creating the user > redirect to homepage
 	c.JSON(http.StatusOK, gin.H{"message": "Register done..."})
 }
